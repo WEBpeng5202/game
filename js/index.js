c4a1736b17c43fd8b57f52c44a8ed98f244ecb14
dengpeng_5202 <1006125584@qq.com>
2017-12-20 08:31:03 +0800

    chushihua

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 window.onload=function () {
-    let code=new Code(1,Math.floor(Math.random()*10+1));
+    let code=new Code(1,Math.floor(Math.random()+1));
     code.start();
+    document.onclick=function () {
+        code.Time();
+    }
 };
